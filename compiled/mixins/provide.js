21802653b93b129de2f0a7eb9b89674136985547
Jakub Tymoszuk <jtymoszuk97@gmail.com>
2024-08-23 17:21:05 +0200

    debug code

--- compiled/mixins/provide.js
+++ compiled/mixins/provide.js
@@ -121,7 +121,7 @@ module.exports = function () {
       return _this.initialRequestSent;
     },
     setRef: function setRef(key, el) {
-      console.log("++++++++++++++++++++++++++11111111111111++++++++++++++++++++++++++")
+      console.log("++++++++++++++++++++++++++11111111111111++++++++++++++++++++++++++ " + key + ' | ' + el)
       _this.refs[key] = el;
       console.log("++++++++++++++++++++++++++111111111111111++++++++++++++++++++++++++")
     },
