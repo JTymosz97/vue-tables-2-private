1d5ea4e94db11a9cd5bb53ea3713d797239dc1e2
Jakub Tymoszuk <jtymoszuk97@gmail.com>
2025-02-18 15:39:11 +0100

    more monor changes, ref LB-3138

--- compiled/helpers/resizeable-columns.js
+++ compiled/helpers/resizeable-columns.js
@@ -3,6 +3,7 @@
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 module.exports = function (table, hasChildRow, isChildRowTogglerFirst, resizeableColumns, stickyHeader) {
+  console.log("using resizable columns");
   return;
   var row = table.getElementsByTagName("tr")[0],
       cols = row ? Array.from(row.children) : undefined;
